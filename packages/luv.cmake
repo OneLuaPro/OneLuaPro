6ead087988b880da9cc9d0f97f6c1f492d56f592
Kritzel Kratzel <kritzel.kratzel@gmx.de>
2025-03-16 15:00:21 +0100

    Preparations for v5.4.7.5 - not yet done

--- packages/luv.cmake
+++ packages/luv.cmake
@@ -5,7 +5,9 @@ ExternalProject_Add(luv
   GIT_REPOSITORY https://github.com/OneLuaPro/luv.git
   GIT_TAG "origin/master"
   GIT_PROGRESS FALSE
-  CMAKE_ARGS "-DLUA_HINTS=${ONELUAPRO_BUILDROOT}"
+  CMAKE_ARGS
+  "-DUSE_ONELUAPRO=ON" 
+  "-DLUA_HINTS=${ONELUAPRO_BUILDROOT}"
   "-DCMAKE_INSTALL_PREFIX=${ONELUAPRO_PREFIX}"
 )
 ExternalProject_Add_StepDependencies(luv build lua libuv)
