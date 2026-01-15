cc7637a3699fbedbc8275da6d67cfeb382c55ed8
Kritzel Kratzel <kritzel.kratzel@gmx.de>
2026-01-15 09:56:12 +0100

    Added lua_cliargs

---
+++ packages/cliargs.cmake
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------------------------
+# cliargs
+ExternalProject_Add(cliargs
+  PREFIX ${PROJECT_NAME}/cliargs
+  GIT_REPOSITORY https://github.com/OneLuaPro/cliargs.git
+  GIT_TAG "origin/master"
+  GIT_PROGRESS FALSE
+  CMAKE_ARGS "-DLUA_HINTS=${ONELUAPRO_BUILDROOT}"
+  "-DCMAKE_INSTALL_PREFIX=${ONELUAPRO_PREFIX}"
+)
+ExternalProject_Add_StepDependencies(cliargs build lua)
