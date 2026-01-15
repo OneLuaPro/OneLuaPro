ba2ddbbb9a1429ea69babca83c164442f641ee51
Kritzel Kratzel <kritzel.kratzel@gmx.de>
2026-01-15 13:15:42 +0100

    Repository address fix

--- packages/cliargs.cmake
+++ packages/cliargs.cmake
@@ -2,7 +2,7 @@
 # cliargs
 ExternalProject_Add(cliargs
   PREFIX ${PROJECT_NAME}/cliargs
-  GIT_REPOSITORY https://github.com/OneLuaPro/cliargs.git
+  GIT_REPOSITORY https://github.com/OneLuaPro/lua_cliargs.git
   GIT_TAG "origin/master"
   GIT_PROGRESS FALSE
   CMAKE_ARGS "-DLUA_HINTS=${ONELUAPRO_BUILDROOT}"
